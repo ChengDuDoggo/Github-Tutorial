17a4849cf1cd364b9e682ab5d90a108f2957bcb0
ChengDuDoggo <382782974@qq.com>
2022-08-17 15:59:51 +0800

    Hallo lisi

--- Assets/Script/PlayerContraler.cs
+++ Assets/Script/PlayerContraler.cs
@@ -7,7 +7,7 @@ public class PlayerContraler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print($"Hallo World!");
+        print($"Hallo lisi!");
     }
 
     // Update is called once per frame
